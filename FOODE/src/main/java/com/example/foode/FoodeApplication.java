ffe8985fd7473d7af35d9fb78fcc1530bf83b5f8
Dawid Dudek <dawid.dudek@fingo.pl>
2022-04-25 16:06:00 +0200

    init project structure

---
+++ FOODE/src/main/java/com/example/foode/FoodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.foode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodeApplication.class, args);
+	}
+
+}
