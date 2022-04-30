df3d8b40d23bd1f6627cd954e791148d0542c33d
Lerqiu <mcwk810@gmail.com>
2022-04-30 13:47:32 +0200

    Basic routing

---
+++ frontend/src/ProductsOffers/ProductsOffers.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+
+function ProductsOffers() {
+    return (
+       <h1>
+           Raw product
+       </h1>
+    );
+}
+
+export default ProductsOffers;
