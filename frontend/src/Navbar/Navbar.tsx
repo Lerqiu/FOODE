7a4164ac27db7de53bc6b6a5f1dfba25b6fa7e29
Lerqiu <43934889+Lerqiu@users.noreply.github.com>
2022-04-30 16:08:16 +0200

    Basic routing (#3)
    * Basic routing
    
    * Changing to arrow function

---
+++ frontend/src/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <>
+            <div style={{ height: '100px', width: '100%', backgroundColor: 'red' } as React.CSSProperties}>
+                <h1>Raw navbar</h1>
+            </div>
+            <Outlet />
+        </>
+    );
+}
+
+export default Navbar;
---
+++ frontend/src/ProductsOffers/ProductsOffers.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom';
+
+const ProductsOffers = () => {
+    return (
+        <h1>
+            Raw product
+        </h1>
+    );
+}
+
+export default ProductsOffers;
