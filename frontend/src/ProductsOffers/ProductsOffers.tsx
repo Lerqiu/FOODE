4c1e3073b4634dcbee5de552a18e19fa9d3aeffa
Lerqiu <mcwk810@gmail.com>
2022-05-01 02:30:24 +0200

    Add navbar

--- frontend/src/ProductsOffers/ProductsOffers.tsx
+++ frontend/src/ProductsOffers/ProductsOffers.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
+import NavbarButton from '../Navbar/Button/NavbarButton';
 
 const ProductsOffers = () => {
     return (
         <h1>
-            Raw product
+          Example text
         </h1>
     );
 }
