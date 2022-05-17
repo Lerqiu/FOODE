83b3ed9ad4ddf5abf9e77ccda98d3b068e89d1c7
MiSobecki <michal2000sob@gmail.com>
2022-05-18 00:02:16 +0100

    Delete useless interface ProductService.

--- src/main/java/com/example/foode/Product/ProductService.java
+++ src/main/java/com/example/foode/Product/ProductService.java
@@ -1,20 +1,52 @@
 package com.example.foode.Product;
 
+import com.example.foode.Product.Exception.ProductNotFoundException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
-public interface ProductService {
+@Service
+@RequiredArgsConstructor
+public class ProductService {
 
-    Product createProduct(Product product);
+    private final ProductRepository productRepository;
 
-    Page<Product> getProductsByName(String name,
-                                    Pageable pageable);
+    public Product createProduct(Product product) {
+        return productRepository.saveAndFlush(product);
+    }
 
-    Product getProduct(Long id);
+    public Page<Product> getProductsByName(String name,
+                                           Pageable pageable) {
+        return productRepository.findAllByNameContainingIgnoreCase(name, pageable);
+    }
 
-    void deleteProduct(Long id);
+    public Product getProduct(Long id) {
+        return productRepository.findById(id)
+                .orElseThrow(() -> new ProductNotFoundException(id));
+    }
 
-    Product updateProduct(Product newProduct,
-                          Long id);
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
 
+    public Product updateProduct(Product newProduct,
+                                 Long id) {
+        return productRepository.findById(id)
+                .map(product -> cloneAndSaveProduct(product, newProduct))
+                .orElseGet(() -> saveNewProduct(newProduct, id));
+    }
+
+    private Product cloneAndSaveProduct(Product fromProduct,
+                                        Product toProduct) {
+        toProduct.setExpirationDate(fromProduct.getExpirationDate());
+        toProduct.setName(fromProduct.getName());
+        return productRepository.saveAndFlush(toProduct);
+    }
+
+    private Product saveNewProduct(Product product,
+                                   Long id) {
+        product.setId(id);
+        return productRepository.saveAndFlush(product);
+    }
 }
