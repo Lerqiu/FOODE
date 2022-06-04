b6b3806316160066030eab21cebe429bd216d7ee
Dawid Dudek <dawid.dudek@fingo.pl>
2022-06-04 19:44:33 +0200

    added formik modal

--- frontend/src/components/Offers/AddOfferModal/AddOfferModal.tsx
+++ frontend/src/components/Offers/AddOfferModal/AddOfferModal.tsx
@@ -1,27 +1,19 @@
 import React, { useState } from 'react';
 import {
-  Button,
-  Form, FormControl, FormLabel, Modal,
+  Modal,
 } from 'react-bootstrap';
 import 'react-datepicker/dist/react-datepicker.css';
-// eslint-disable-next-line import/no-named-default
-import { default as ReactDatePicker } from 'react-datepicker';
 import { useQuery, useQueryClient } from 'react-query';
 import OfferService from '../../../services/OfferService';
 import ICity from '../../../interfaces/city/ICity';
 import CityService from '../../../services/CityService';
+import AddOfferForm from '../AddOfferForm/AddOfferForm';
 
 function AddOfferModal(props: {show: any, onHide: any}) {
   const { show, onHide } = props;
   const [cities, setCities] = useState<ICity[]>([]);
 
   const title = 'Dodaj oferte';
-  const [price, setPrice] = useState('');
-  const [city, setCity] = useState();
-  const [description, setDescription] = useState('');
-  const [availability, setAvailability] = useState('');
-  const [productName, setProductName] = useState('');
-  const [expirationDate, setExpirationDate] = useState(new Date());
 
   const queryClient = useQueryClient();
 
@@ -39,36 +31,24 @@ function AddOfferModal(props: {show: any, onHide: any}) {
   );
 
   const addOffer = (offer: any) => {
-    OfferService.save(offer)
-      .then(() => queryClient.invalidateQueries('offers-get'));
-  };
+    const product = {
+      name: offer.productName,
+      expirationDate: offer.expirationDate,
+    };
 
-  const clearModal = () => {
-    setPrice('');
-    setCity(undefined);
-    setDescription('');
-    setAvailability('');
-    setProductName('');
-    setExpirationDate(new Date());
-  };
+    console.log(offer);
 
-  const handleAddOffer = () => {
-    const offer = {
-      price: parseInt(price, 10),
+    const offerToSend = {
+      ...offer,
       date: new Date(),
-      city,
-      description,
-      availability,
       user: {
         id: 1,
       },
-      product: {
-        name: productName,
-        expirationDate,
-      },
+      product,
+      city: JSON.parse(offer.city),
     };
-    addOffer(offer);
-    clearModal();
+    OfferService.save(offerToSend)
+      .then(() => queryClient.invalidateQueries('offers-get'));
     onHide();
   };
 
@@ -85,76 +65,13 @@ function AddOfferModal(props: {show: any, onHide: any}) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <Form>
-          <FormLabel>Nazwa produktu</FormLabel>
-          <FormControl
-            value={productName}
-            onChange={(event) => setProductName(event.target.value)}
-            placeholder="nazwa produktu"
-          />
-          <FormLabel>Cena</FormLabel>
-          <FormControl
-            value={price}
-            onChange={(event) => setPrice(event.target.value)}
-            placeholder="Cena"
-          />
-          <FormLabel>Miasto</FormLabel>
-          <Form.Select
-            defaultValue="PLACEHOLDER"
-            onChange={(event) => setCity(JSON.parse(event.target.value))}
-          >
-            {cities ? cities.map((_city) => (
-              <option key={_city.id} value={JSON.stringify(_city)}>
-                {_city.name}
-              </option>
-            )) : ''}
-          </Form.Select>
-          <Form.Group
-            className="mb-3"
-            controlId="exampleForm.ControlTextarea1"
-          >
-            <Form.Label>Opis</Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={3}
-              value={description}
-              onChange={(event) => setDescription(event.target.value)}
-              placeholder="Opis"
-            />
-          </Form.Group>
-          <FormLabel>Dostępność</FormLabel>
-          <FormControl
-            value={availability}
-            onChange={(event) => setAvailability(event.target.value)}
-            placeholder="dostępność"
-          />
-          <FormLabel>Data ważności</FormLabel>
-          <ReactDatePicker
-            dateFormat="yyyy-MM-dd"
-            showPopperArrow={false}
-            minDate={new Date()}
-            selected={expirationDate}
-            onChange={(date) => setExpirationDate(date!)}
-            useWeekdaysShort
-            fixedHeight
-          />
-        </Form>
+        <AddOfferForm
+          cities={cities}
+          submit={addOffer}
+        />
       </Modal.Body>
-      <Modal.Footer>
-        <Button
-          variant="secondary"
-          onClick={handleAddOffer}
-        >
-          Close
-        </Button>
-        <Button
-          variant="primary"
-          onClick={handleAddOffer}
-        >
-          Save Changes
-        </Button>
-      </Modal.Footer>
     </Modal>
+  // <AddOfferForm cities={cities} submit={(e) => console.log(e)} />
   );
 }
 
