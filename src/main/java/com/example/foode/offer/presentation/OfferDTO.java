100b9f1461c54df6ce01c8eb71cb36fa613348fa
MiSobecki <58309482+MiSobecki@users.noreply.github.com>
2022-06-19 13:03:47 +0200

    Add be validation (#31)
    * Implement validation for fields.
    
    * Add @Null case for offer id.
    
    * Add integration tests and minor fixes.
    
    * Change record to class.

--- src/main/java/com/example/foode/offer/presentation/OfferDTO.java
+++ src/main/java/com/example/foode/offer/presentation/OfferDTO.java
@@ -1,27 +1,46 @@
 package com.example.foode.offer.presentation;
 
 import com.example.foode.city.presentation.CityDTO;
+import com.example.foode.offer.presentation.crudmarker.OnOfferCreate;
 import com.example.foode.product.presentation.ProductDTO;
 import lombok.Data;
 
+import javax.validation.Valid;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
 public class OfferDTO {
+    @Positive(message = "Id must be positive")
+    @Null(groups = OnOfferCreate.class, message = "Id must be null if we want to create a new offer")
     private Long id;
 
+    @NotNull
+    @PositiveOrZero(message = "Price must be non-negative")
     private BigDecimal price;
 
+    @NotNull
+    @PastOrPresent(message = "Offer publication date should be from past or present")
     private LocalDate date;
 
+    @NotNull
+    @Valid
     private CityDTO city;
 
+    @NotBlank(message = "Description cannot be blank")
+    @Size(max = 4000, message = "Description must be of max size 4000")
     private String description;
 
+    @NotBlank(message = "Availability cannot be blank")
+    @Size(max = 255, message = "Availability must be of max size 255")
     private String availability;
 
+    @NotNull
+    @Valid
     private ProductDTO product;
 
+    @NotNull
+    @Valid
     private UserOfferDTO userOutput;
 }
