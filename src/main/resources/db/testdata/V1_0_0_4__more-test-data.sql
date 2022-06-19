42b69a72e4b0640d096a973e814707f3f19caee3
szymeq17 <rysz.szymon@gmail.com>
2022-06-19 14:53:08 +0200

    Fix test offers SQL script

--- src/main/resources/db/testdata/V1_0_0_4__more-test-data.sql
+++ src/main/resources/db/testdata/V1_0_0_4__more-test-data.sql
@@ -1,108 +1,108 @@
--- INSERT INTO product
--- VALUES (NEXT VALUE FOR products_id_seq, 'płatki śniadaniowe', '2022-08-07');
---
--- INSERT INTO product
--- VALUES (NEXT VALUE FOR products_id_seq, 'paprykarz szczeciński', '2022-08-08');
---
--- INSERT INTO product
--- VALUES (NEXT VALUE FOR products_id_seq, 'coca cola', '2022-09-01');
---
--- INSERT INTO product
--- VALUES (NEXT VALUE FOR products_id_seq, 'sok pomarańczowy', '2022-09-10');
---
--- INSERT INTO product
--- VALUES (NEXT VALUE FOR products_id_seq, 'gruszka', '2022-10-07');
---
--- INSERT INTO product
--- VALUES (NEXT VALUE FOR products_id_seq, 'ser', '2022-09-07');
---
--- INSERT INTO product
--- VALUES (NEXT VALUE FOR products_id_seq, 'ketchup', '2022-08-07');
---
--- INSERT INTO product
--- VALUES (NEXT VALUE FOR products_id_seq, 'musztarda', '2022-08-08');
---
--- INSERT INTO product
--- VALUES (NEXT VALUE FOR products_id_seq, 'chipsy lays zielona cebulka', '2022-09-01');
---
--- INSERT INTO product
--- VALUES (NEXT VALUE FOR products_id_seq, 'olej rzepakowy', '2022-09-10');
---
--- INSERT INTO product
--- VALUES (NEXT VALUE FOR products_id_seq, 'mąka', '2022-10-07');
---
--- INSERT INTO product
--- VALUES (NEXT VALUE FOR products_id_seq, 'makaron', '2022-09-07');
---
--- INSERT INTO product
--- VALUES (NEXT VALUE FOR products_id_seq, 'płatki śniadaniowe', '2022-08-07');
---
--- INSERT INTO product
--- VALUES (NEXT VALUE FOR products_id_seq, 'paprykarz szczeciński', '2022-08-08');
---
--- INSERT INTO product
--- VALUES (NEXT VALUE FOR products_id_seq, 'coca cola', '2022-09-01');
---
--- INSERT INTO product
--- VALUES (NEXT VALUE FOR products_id_seq, 'sok pomarańczowy', '2022-09-10');
---
--- INSERT INTO product
--- VALUES (NEXT VALUE FOR products_id_seq, 'gruszka', '2022-10-07');
---
--- INSERT INTO product
--- VALUES (NEXT VALUE FOR products_id_seq, 'ser', '2022-09-07');
+INSERT INTO product
+VALUES (NEXT VALUE FOR products_id_seq, 'płatki śniadaniowe', '2022-08-07');
+
+INSERT INTO product
+VALUES (NEXT VALUE FOR products_id_seq, 'paprykarz szczeciński', '2022-08-08');
+
+INSERT INTO product
+VALUES (NEXT VALUE FOR products_id_seq, 'coca cola', '2022-09-01');
+
+INSERT INTO product
+VALUES (NEXT VALUE FOR products_id_seq, 'sok pomarańczowy', '2022-09-10');
+
+INSERT INTO product
+VALUES (NEXT VALUE FOR products_id_seq, 'gruszka', '2022-10-07');
+
+INSERT INTO product
+VALUES (NEXT VALUE FOR products_id_seq, 'ser', '2022-09-07');
+
+INSERT INTO product
+VALUES (NEXT VALUE FOR products_id_seq, 'ketchup', '2022-08-07');
+
+INSERT INTO product
+VALUES (NEXT VALUE FOR products_id_seq, 'musztarda', '2022-08-08');
+
+INSERT INTO product
+VALUES (NEXT VALUE FOR products_id_seq, 'chipsy lays zielona cebulka', '2022-09-01');
+
+INSERT INTO product
+VALUES (NEXT VALUE FOR products_id_seq, 'olej rzepakowy', '2022-09-10');
+
+INSERT INTO product
+VALUES (NEXT VALUE FOR products_id_seq, 'mąka', '2022-10-07');
+
+INSERT INTO product
+VALUES (NEXT VALUE FOR products_id_seq, 'makaron', '2022-09-07');
+
+INSERT INTO product
+VALUES (NEXT VALUE FOR products_id_seq, 'płatki śniadaniowe', '2022-08-07');
+
+INSERT INTO product
+VALUES (NEXT VALUE FOR products_id_seq, 'paprykarz szczeciński', '2022-08-08');
+
+INSERT INTO product
+VALUES (NEXT VALUE FOR products_id_seq, 'coca cola', '2022-09-01');
+
+INSERT INTO product
+VALUES (NEXT VALUE FOR products_id_seq, 'sok pomarańczowy', '2022-09-10');
+
+INSERT INTO product
+VALUES (NEXT VALUE FOR products_id_seq, 'gruszka', '2022-10-07');
+
+INSERT INTO product
+VALUES (NEXT VALUE FOR products_id_seq, 'ser', '2022-09-07');
 
 
 INSERT INTO offer
-VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 1, 8);
+VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 7, 8);
 
 INSERT INTO offer
-VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 2, 8);
+VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 8, 8);
 
 INSERT INTO offer
-VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 3, 10);
+VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 9, 10);
 
 INSERT INTO offer
-VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 4, 9);
+VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 10, 9);
 
 INSERT INTO offer
-VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 5, 9);
+VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 11, 9);
 
 INSERT INTO offer
-VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 6, 8);
+VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 12, 8);
 
 INSERT INTO offer
-VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 1, 7);
+VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 13, 7);
 
 INSERT INTO offer
-VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 2, 6);
+VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 14, 6);
 
 INSERT INTO offer
-VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 3, 5);
+VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 15, 5);
 
 INSERT INTO offer
-VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 4, 4.60);
+VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 16, 4.60);
 
 INSERT INTO offer
-VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 5, 2.11);
+VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 17, 2.11);
 
 INSERT INTO offer
-VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 6, 2.34);
+VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 1, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 18, 2.34);
 
 INSERT INTO offer
-VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 2 , 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 1, 1.50);
+VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 2 , 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 19, 1.50);
 
 INSERT INTO offer
-VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 2, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 2, 22);
+VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 2, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 20, 22);
 
 INSERT INTO offer
-VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 2, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 3, 18);
+VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 2, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 21, 18);
 
 INSERT INTO offer
-VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 2, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 4, 4.30);
+VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 2, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 22, 4.30);
 
 INSERT INTO offer
-VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 2, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 5, 2.30);
+VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 2, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 23, 2.30);
 
 INSERT INTO offer
-VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 2, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 6, 3.50);
+VALUES (NEXT VALUE FOR offers_id_seq, '2022-06-07', 2, 'Nie za długi, ale też nie za krótki opis produktu', 'availability 1', 1, 24, 3.50);
