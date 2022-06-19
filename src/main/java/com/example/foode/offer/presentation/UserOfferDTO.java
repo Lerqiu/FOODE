f08e3f51ae5e9c10fa24318841cab07b8b3a5914
MiSobecki <michal2000sob@gmail.com>
2022-06-19 11:59:42 +0100

    Change record to class.

--- src/main/java/com/example/foode/offer/presentation/UserOfferDTO.java
+++ src/main/java/com/example/foode/offer/presentation/UserOfferDTO.java
@@ -1,18 +1,37 @@
 package com.example.foode.offer.presentation;
 
 import com.example.foode.user.User;
+import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
-public record UserOfferDTO(@NotNull @Positive(message = "Id must be positive") Long id,
-                           @NotBlank(message = "Login cannot be blank") String login,
-                           @NotBlank(message = "Contact cannot be blank") String contact){
+@Data
+public final class UserOfferDTO {
 
-    public static UserOfferDTO from(User user){
-        return new UserOfferDTO(user.getId(),
-                                    user.getLogin(),
-                                    user.getContact());
+    @NotNull
+    @Positive(message = "Id must be positive")
+    private Long id;
+
+    @NotBlank(message = "Login cannot be blank")
+    private String login;
+
+    @NotBlank(message = "Contact cannot be blank")
+    private String contact;
+
+    public UserOfferDTO(Long id,
+                        String login,
+                        String contact) {
+        this.id = id;
+        this.login = login;
+        this.contact = contact;
     }
+
+    public static UserOfferDTO from(User user) {
+        return new UserOfferDTO(user.getId(),
+                user.getLogin(),
+                user.getContact());
+    }
+
 }
