fd8ff0b6073f24f526bddbc3314f8e16218a911c
szymeq17 <rysz.szymon@gmail.com>
2022-06-19 14:49:39 +0200

    Add description component

---
+++ frontend/src/components/Offers/OfferList/Offer/OfferDescription/OfferDescription.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function OfferDescription(props: { description: string }) {
+    const { description } = props;
+    return (
+        <div style={
+            {
+                color: 'white',
+                backgroundColor: '#82bc75',
+                borderRadius: '10px',
+                padding: '5px',
+                fontSize: '0.7rem',
+                height: '100%'
+            }
+        }>
+            {description}
+        </div>
+    );
+}
+
+export default OfferDescription;
